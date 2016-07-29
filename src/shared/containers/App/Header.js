27ece97ea321807cfcc9522190d2b45d9dc3b726
Suranart Niamcome <suranartnc@gmail.com>
2016-07-29 13:23:28 +0700

    add simple components in Header

--- src/shared/containers/App/Header.js
+++ src/shared/containers/App/Header.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class Header extends Component {
 
   render() {
     return (
-      <div>
-        Header
-      </div>
+      <header>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12 col-md-4">
+              <Link to={`/`}>
+                Logo
+              </Link>
+            </div>
+            <div className="col-xs-12 col-md-8">
+              <div className="pull-right">
+                Login
+              </div>
+              <div className="pull-right">
+                Search
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     );
   }
 }
