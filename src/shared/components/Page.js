d3ddac5ba7a395aade2a8f6e5bcc9459508c6140
SuranartN <suranartnc@gmail.com>
2016-07-30 21:10:31 +0700

    refactor functional component

--- src/shared/components/Page.js
+++ src/shared/components/Page.js
@@ -4,8 +4,8 @@ import Header from 'shared/containers/App/Header';
 const createPage = (initialState = {}) => (BaseComponent) => {
 	class Page extends Component {
 		constructor(props) {
-		    super(props);
-		    this.state = initialState;
+	    super(props);
+	    this.state = initialState;
 		}
 		render() {
 			if (this.state.mode === 'full') {
@@ -16,14 +16,14 @@ const createPage = (initialState = {}) => (BaseComponent) => {
 			return (
 				<div>
 				  <Header />
-		          <div className="container">
-		            <BaseComponent {...this.props} />
-		          </div>
-		        </div>
+          <div className="container">
+            <BaseComponent {...this.props} />
+          </div>
+        </div>
 			);
 		}
 	}
-  	return Page;
+	return Page;
 }
 
 export default createPage;
--- src/shared/components/Post.js
+++ src/shared/components/Post.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Link } from 'react-router';
 import styles from './Post.scss';
 
-export default (props) => {
-	const { post } = props;
-	return (
-		<article className={styles['article']}>
-      <h3><Link to={`/post/${post.id}`}>{post.title}</Link></h3>
-    </article>
-	);
-}
+const Post = ({post}) => (
+	<article className={styles['article']}>
+    <h3><Link to={`/post/${post.id}`}>{post.title}</Link></h3>
+  </article>
+);
+
+export default Post;
