44281493687f8451dbbc1f0998d5f1017651ca84
Suranart Niamcome <suranartnc@gmail.com>
2016-08-02 21:19:31 +0700

    replace redux devtools with redux logger

--- src/shared/containers/Root.js
+++ src/shared/containers/Root.js
@@ -1,30 +1,16 @@
 import config from 'shared/configs';
 
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import { Router } from 'react-router/es6'
-import { syncHistoryWithStore } from 'react-router-redux'
-import createStore from 'shared/store/createStore'
-import createRoutes from 'shared/routes'
-import { DevTools } from 'shared/components';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router/es6';
+import { syncHistoryWithStore } from 'react-router-redux';
+import createStore from 'shared/store/createStore';
+import createRoutes from 'shared/routes';
 
 export default class Root extends Component {
   render() {
     const { history } = this.props;
     const store = createStore(history);
-
-    if (!config.isProduction) {
-      return (
-        <Provider store={store} key='provider'>
-          <div>
-            <Router 
-              history={syncHistoryWithStore(history, store)}
-              routes={createRoutes(store)} />
-            <DevTools /> 
-          </div>
-        </Provider>
-      )
-    }
 
     return (
       <Provider store={store} key='provider'>
