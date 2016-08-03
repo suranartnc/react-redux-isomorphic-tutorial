878ecc89214e246cbb563439d36debdf5d52b991
Suranart Niamcome <suranartnc@gmail.com>
2016-08-03 20:36:53 +0700

    add some rules

--- src/client.js
+++ src/client.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
-import { browserHistory } from 'react-router/es6'
-import { AppContainer } from 'react-hot-loader'
-import Root from 'shared/containers/Root'
+import React from 'react';
+import { render } from 'react-dom';
+import { browserHistory } from 'react-router/es6';
+import { AppContainer } from 'react-hot-loader';
+import Root from 'shared/containers/Root';
 
-const rootEl = document.getElementById('root')
+const rootEl = document.getElementById('root');
 
 render(
   <AppContainer>
     <Root history={browserHistory} />
   </AppContainer>,
   rootEl
-)
+);
 
 if (module.hot) {
   module.hot.accept('shared/containers/Root', () => {
-    const NextRootApp = require('shared/containers/Root').default
+    const NextRootApp = require('shared/containers/Root').default;
 
     render(
       <AppContainer>
-         <NextRootApp history={browserHistory} />
+        <NextRootApp history={browserHistory} />
       </AppContainer>,
       rootEl
-    )
-  })
+    );
+  });
 }
