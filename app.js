086bad401acc39eb03215c4cbcc81c3dae171831
SuranartN <suranartnc@gmail.com>
2016-08-08 22:41:30 +0700

    add css modules require hook

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 require('babel-register');
+require('css-modules-require-hook')({
+  extensions: ['.css', '.scss'],
+  generateScopedName: '[name]__[local]___[hash:base64:5]'
+});
 
 require('./api/server');
 require('./src/server');
