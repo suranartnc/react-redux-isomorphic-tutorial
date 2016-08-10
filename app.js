14166c655b6c42605d92f27c893ace5c33a92172
SuranartN <suranartnc@gmail.com>
2016-08-11 00:39:33 +0700

    adopt babel-plugin-webpack-loaders

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-require('babel-register');
-require('css-modules-require-hook')({
-  extensions: ['.css', '.scss'],
-  generateScopedName: '[name]__[local]___[hash:base64:5]'
+// require('babel-register');
+// require('css-modules-require-hook')({
+//   extensions: ['.css', '.scss'],
+//   generateScopedName: '[name]__[local]___[hash:base64:5]'
+// });
+
+require('babel-register')({
+  "plugins": [
+    [
+      "babel-plugin-webpack-loaders",
+      {
+        "config": "./webpack/webpack.config.babel.js",
+        "verbose": false
+      }
+    ]
+  ]
 });
 
 require('./api/server');
---
+++ webpack/webpack.config.babel.js
@@ -0,0 +1,53 @@
+var path = require('path');
+
+module.exports = {
+
+  output: {
+    libraryTarget: 'commonjs2'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ]
+      }, {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            query: {
+              module: true,
+              importLoaders: 2,
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          },
+          'sass-loader',
+          'postcss-loader'
+        ]
+      }, {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.json', '.js', '.jsx'],
+    modules: [
+      path.join(process.cwd(), 'src'),
+      path.join(process.cwd(), 'node_modules')
+    ]
+  },
+
+  postcss: [],
+
+  sassLoader: {
+    includePaths: [path.resolve(process.cwd(), "src/shared/theme/scss")]
+  }
+};
