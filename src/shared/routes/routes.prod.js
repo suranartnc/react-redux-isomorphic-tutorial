b810f8c97a6fae600c1c63b2db97d57d9b2a60ac
SuranartN <suranartnc@gmail.com>
2016-08-12 16:58:05 +0700

    remove old routes

--- src/shared/routes/routes.prod.js
+++ src/shared/routes/routes.prod.js
@@ -3,23 +3,6 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from 'shared/containers/App/App';
 
-// if (typeof System === "undefined") {
-//   var System = {
-//     import: function(path) {
-//       return Promise.resolve(require(path));
-//     }
-//   };
-// }
-
-// const errorLoading = (err) => {
-//   console.error('Dynamic page loading failed', err);
-// };
-
-// const loadModule = (cb) => (componentModule) => {
-//   cb(null, componentModule.default);
-// };
-
-// require.ensure polyfill for node
 if (typeof require.ensure !== 'function') {
   require.ensure = function requireModule(deps, callback) {
     callback(require);
@@ -27,36 +10,13 @@ if (typeof require.ensure !== 'function') {
 }
 
 export default function createRoutes(store) {
-  // return [
-  //   {
-  //     path: '/',
-  //     component: App,
-  //     indexRoute: {
-  //       getComponent(nextState, cb) {
-  //         System.import('shared/containers/HomePage/HomePage')
-  //           .then(loadModule(cb))
-  //           .catch(errorLoading);
-  //       }         
-  //     },
-  //     childRoutes: [
-  //       {
-  //         path: 'post/:id',
-  //         getComponent(nextState, cb) {
-  //           System.import('shared/containers/EntryPage/EntryPage')
-  //             .then(loadModule(cb))
-  //             .catch(errorLoading);
-  //         }
-  //       }
-  //     ]
-  //   }
-  // ]
   return (
     <Route path="/" component={App}>
       <IndexRoute
         getComponent={(nextState, cb) => {
           require.ensure([], require => {
             cb(null, require('shared/containers/HomePage/HomePage').default);
-          });
+          }, 'home');
         }}
       />
       <Route
@@ -64,7 +24,7 @@ export default function createRoutes(store) {
         getComponent={(nextState, cb) => {
           require.ensure([], require => {
             cb(null, require('shared/containers/EntryPage/EntryPage').default);
-          });
+          }, 'entry');
         }}
       />
     </Route>
