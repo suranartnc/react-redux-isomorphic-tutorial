15a92e343d040e5d6de9e43b71e706d5ab6b58da
SuranartN <suranartnc@gmail.com>
2016-08-12 16:31:41 +0700

    add async routes using JSX

--- src/shared/routes/routes.prod.js
+++ src/shared/routes/routes.prod.js
@@ -1,43 +1,72 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
 import App from 'shared/containers/App/App';
 
-if (typeof System === "undefined") {
-  var System = {
-    import: function(path) {
-      return Promise.resolve(require(path));
-    }
+// if (typeof System === "undefined") {
+//   var System = {
+//     import: function(path) {
+//       return Promise.resolve(require(path));
+//     }
+//   };
+// }
+
+// const errorLoading = (err) => {
+//   console.error('Dynamic page loading failed', err);
+// };
+
+// const loadModule = (cb) => (componentModule) => {
+//   cb(null, componentModule.default);
+// };
+
+// require.ensure polyfill for node
+if (typeof require.ensure !== 'function') {
+  require.ensure = function requireModule(deps, callback) {
+    callback(require);
   };
 }
 
-const errorLoading = (err) => {
-  console.error('Dynamic page loading failed', err);
-};
-
-const loadModule = (cb) => (componentModule) => {
-  cb(null, componentModule.default);
-};
-
 export default function createRoutes(store) {
-  return [
-    {
-      path: '/',
-      component: App,
-      indexRoute: {
-        getComponent(nextState, cb) {
-          System.import('shared/containers/HomePage/HomePage')
-            .then(loadModule(cb))
-            .catch(errorLoading);
-        }         
-      },
-      childRoutes: [
-        {
-          path: 'post/:id',
-          getComponent(nextState, cb) {
-            System.import('shared/containers/EntryPage/EntryPage')
-              .then(loadModule(cb))
-              .catch(errorLoading);
-          }
-        }
-      ]
-    }
-  ]
+  // return [
+  //   {
+  //     path: '/',
+  //     component: App,
+  //     indexRoute: {
+  //       getComponent(nextState, cb) {
+  //         System.import('shared/containers/HomePage/HomePage')
+  //           .then(loadModule(cb))
+  //           .catch(errorLoading);
+  //       }         
+  //     },
+  //     childRoutes: [
+  //       {
+  //         path: 'post/:id',
+  //         getComponent(nextState, cb) {
+  //           System.import('shared/containers/EntryPage/EntryPage')
+  //             .then(loadModule(cb))
+  //             .catch(errorLoading);
+  //         }
+  //       }
+  //     ]
+  //   }
+  // ]
+  return (
+    <Route path="/" component={App}>
+      <IndexRoute
+        getComponent={(nextState, cb) => {
+          require.ensure([], require => {
+            cb(null, require('shared/containers/HomePage/HomePage').default);
+          });
+        }}
+      />
+      <Route
+        path="post/:id"
+        getComponent={(nextState, cb) => {
+          require.ensure([], require => {
+            cb(null, require('shared/containers/EntryPage/EntryPage').default);
+          });
+        }}
+      />
+    </Route>
+  );
 }
